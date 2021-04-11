df9522afd5fd3a0a6b463133fcbc819f3a2de765
Anika Shetty <anikashetty@Anikas-MacBook-Pro.local>
2021-04-11 16:12:19 -0700

    Improved search function

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,9 +1,19 @@
-class Illness < ApplicationRecord::Base
+class Illness < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :search_any_symptoms,
+  pg_search_scope :search_by_symptoms,
                   against: :symptoms,
                   using: {
-                    tsearch: {any_symptoms: true}
+                    tsearch: {
+                      any_word: true,
+                      negation: true,
+                      dictionary: "english"
+                    },
+                    dmetaphone: {
+                      any_word: true
+                    },
+                    trigram: {
+                      threshold: 0.1
+                    }
                   }
 
   pg_search_scope :search_all_symptoms,
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_035013) do
+ActiveRecord::Schema.define(version: 2021_04_11_224830) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "illnesses", force: :cascade do |t|
