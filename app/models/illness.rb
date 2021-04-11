14cd25040c22666117a08a381df238b94618a193
Anika Shetty <anikashetty@Anikas-MacBook-Pro.local>
2021-04-11 14:08:51 -0700

    add any search

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,9 +1,11 @@
 class Illness < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :search_by_symptoms, against: :symptoms
-  using: {
+  pg_search_scope :search_any_symptoms,
+                  against: :symptoms
+                  using: {
                     tsearch: {any_word: true}
                   }
 
-  pg_search_scope :search_by_symptoms, against: :symptoms
+  pg_search_scope :search_all_symptoms,
+                  against: :symptoms
 end
