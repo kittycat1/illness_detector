4bc41db0e97f178abfc909c56f2e6652d7dedbac
Anika Shetty <anikashetty@Anikas-MacBook-Pro.local>
2021-04-11 13:48:06 -0700

    any word search

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,4 +1,9 @@
 class Illness < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_by_symptoms, against: :symptoms
+  using: {
+                    tsearch: {any_word: true}
+                  }
+
+                  pg_search_scope :search_by_symptoms, against: :symptoms
 end
